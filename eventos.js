5162043740e9efc480cf283502568d0e29485af5
Nicole Alejandra Aranda Machado <nicolaranda12345678@gmail.com>
2025-08-19 13:18:06 -0300

    Evitar la propagación del click en el botón

--- eventos.js
+++ eventos.js
@@ -2,3 +2,8 @@ const divBoton = document.getElementById("button");
 divBoton.addEventListener("click", function (){
     alert("Hola! Soy el div!")
 });
+const boton = document.getElementById("saludo");
+boton.addEventListener("click", function(event){
+    event.stopPropagation();
+    alert("Hola!");
+});
